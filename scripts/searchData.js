6516f3e60e1d74baef1df365b6e429a5963e0983
cake-lier <cake-lier@users.noreply.github.com>
2022-11-30 22:34:32 +0000

    Deploying to gh-pages from @ pervasive-cats/example@cd0ca3c3d816c462fea88df3560b265087eadd39 🚀

---
+++ scripts/searchData.js
@@ -0,0 +1,4 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"example","t":"example","d":"","k":"static"},
+{"l":"io/github/pervasivecats.html#","e":false,"i":"","n":"io.github.pervasivecats","t":"io.github.pervasivecats","d":"","k":"package"},
+{"l":"io/github/pervasivecats.html#main-94c","e":false,"i":"","n":"main","t":"main(): Unit","d":"io.github.pervasivecats","k":"def"},
+{"l":"io/github/pervasivecats/main.html#","e":false,"i":"","n":"main","t":"main","d":"io.github.pervasivecats","k":"class"}];
