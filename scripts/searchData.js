a9198563123fd612f13d118ab07c1755975431fa
cake-lier <cake-lier@users.noreply.github.com>
2022-12-01 00:00:02 +0000

    Deploying to gh-pages from @ pervasive-cats/example@e60b63dd21ec0d98fac1564007882342ac5f80a1 🚀

--- scripts/searchData.js
+++ scripts/searchData.js
@@ -1,4 +1,7 @@
 pages = [{"l":"index.html#","e":false,"i":"","n":"example","t":"example","d":"","k":"static"},
 {"l":"io/github/pervasivecats.html#","e":false,"i":"","n":"io.github.pervasivecats","t":"io.github.pervasivecats","d":"","k":"package"},
-{"l":"io/github/pervasivecats.html#main-94c","e":false,"i":"","n":"main","t":"main(): Unit","d":"io.github.pervasivecats","k":"def"},
-{"l":"io/github/pervasivecats/main.html#","e":false,"i":"","n":"main","t":"main","d":"io.github.pervasivecats","k":"class"}];
+{"l":"io/github/pervasivecats/Example.html#","e":false,"i":"","n":"Example","t":"Example","d":"io.github.pervasivecats","k":"trait"},
+{"l":"io/github/pervasivecats/Example.html#a-0","e":false,"i":"","n":"a","t":"a: Int","d":"io.github.pervasivecats.Example","k":"val"},
+{"l":"io/github/pervasivecats/Example.html#b-0","e":false,"i":"","n":"b","t":"b: Double","d":"io.github.pervasivecats.Example","k":"val"},
+{"l":"io/github/pervasivecats/Example$.html#","e":false,"i":"","n":"Example","t":"Example","d":"io.github.pervasivecats","k":"object"},
+{"l":"io/github/pervasivecats/Example$.html#apply-fffffa03","e":false,"i":"","n":"apply","t":"apply(a: Int, b: Double): Example","d":"io.github.pervasivecats.Example","k":"def"}];
